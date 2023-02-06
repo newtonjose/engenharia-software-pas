d1fa7bbba105995720f2a9c17b1b9b53d4122ea2
newtonjose <josephnewton001@gmail.com>
2023-02-06 11:53:25 -0300

    wip: simple webxr example

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const activateXR = require('../app/hello-webxr');
+const router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, _next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/hello-webxr', function(req, res, _next) {
+  res.render('webxr/hello-webxr', { title: 'WebXR'});
+});
+
 
 module.exports = router;
